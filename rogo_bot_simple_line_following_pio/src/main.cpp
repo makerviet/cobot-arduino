cb302a01a80d9e1b0f8646b7da368429d83be9ac
Bui Huu An <buihuuan5209@gmail.com>
2023-11-23 20:02:18 +0700

    Update main.cpp

--- rogo_bot_simple_line_following_pio/src/main.cpp
+++ rogo_bot_simple_line_following_pio/src/main.cpp
@@ -10,6 +10,16 @@
 #define right_motor_chanel_a 2
 #define right_motor_chanel_b 3
 
+#define SENSOR_1_PIN 4
+#define SENSOR_2_PIN 3
+#define SENSOR_3_PIN 1
+#define SENSOR_4_PIN 0
+
+#define IR_LED_ON 20
+#define W_LED_ON  21
+
+#define threshold 2000
+
 uint8_t vspeed = 70; //50    
 uint8_t tspeed = 70; //70
 
@@ -76,14 +86,43 @@ void setup() {
   ledcAttachPin(PWM_PIN_L_B, left_motor_chanel_b);
   ledcAttachPin(PWM_PIN_R_A, right_motor_chanel_a);
   ledcAttachPin(PWM_PIN_R_B, right_motor_chanel_b);
+
+  pinMode(W_LED_ON,OUTPUT);
+  pinMode(IR_LED_ON,OUTPUT);
+  digitalWrite(W_LED_ON,1);
+  digitalWrite(IR_LED_ON,1);
 }
 
 void loop() {
-  forward();
+  Serial.print("sensor = \t");
+  Serial.print(analogRead(SENSOR_1_PIN));
+  Serial.print("\t");
+  Serial.print(analogRead(SENSOR_2_PIN));
+  Serial.print("\t");
+  Serial.print(analogRead(SENSOR_3_PIN));
+  Serial.print("\t");
+  Serial.println(analogRead(SENSOR_4_PIN));
 
-  Serial.println(vspeed_l);
-  Serial.print(" ");
-  Serial.print(vspeed_r);
 
-  delay(3000);
+  // Serial.println(vspeed_l);
+  // Serial.print(" ");
+  // Serial.print(vspeed_r);
+
+  if ((analogRead(SENSOR_1_PIN) > threshold  && analogRead(SENSOR_2_PIN) < threshold && analogRead(SENSOR_3_PIN) > threshold && analogRead(SENSOR_4_PIN) > threshold)
+      || (analogRead(SENSOR_1_PIN) > threshold  && analogRead(SENSOR_2_PIN) > threshold && analogRead(SENSOR_3_PIN) < threshold && analogRead(SENSOR_4_PIN) > threshold)
+      || (analogRead(SENSOR_1_PIN) > threshold  && analogRead(SENSOR_2_PIN) < threshold && analogRead(SENSOR_3_PIN) < threshold && analogRead(SENSOR_4_PIN) > threshold))
+    forward();
+  else
+    stop();
+
+  // if (analogRead(SENSOR_2_PIN) > threshold && analogRead(SENSOR_3_PIN) < threshold)
+  //   right();
+
+  // if (analogRead(SENSOR_2_PIN) < threshold && analogRead(SENSOR_3_PIN) > threshold)
+  //   left();
+
+  // if (analogRead(SENSOR_2_PIN) > threshold && analogRead(SENSOR_3_PIN) > threshold)
+  //   forward();
+
+  delay(10);
 }
