2fd70f477610c2f45f56b7b3470b46b6b19d42e5
Bui Huu An <buihuuan5209@gmail.com>
2023-11-26 04:01:24 +0700

    add color sensor to line following

--- rogo_bot_simple_line_following_pio/src/main.cpp
+++ rogo_bot_simple_line_following_pio/src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 #define PWM_PIN_L_A 2
 #define PWM_PIN_L_B 10
 #define PWM_PIN_R_A 6
 #define PWM_PIN_R_B 5
 
-#define left_motor_chanel_a 0
-#define left_motor_chanel_b 1
-#define right_motor_chanel_a 2
-#define right_motor_chanel_b 3
+#define left_motor_channel_a 0
+#define left_motor_channel_b 1
+#define right_motor_channel_a 2
+#define right_motor_channel_b 3
 
 #define SENSOR_1_PIN 4
 #define SENSOR_2_PIN 3
@@ -18,7 +22,7 @@
 #define W_LED_ON 20
 #define IR_LED_ON  21
 
-#define threshold 1900
+#define threshold 2000
 
 uint8_t vspeed = 50; //50    
 uint8_t tspeed = 50; //70
@@ -30,46 +34,46 @@ uint8_t tspeed_r = tspeed * 1.6;
 
 void stop()
 {
-    ledcWrite(left_motor_chanel_a, 0);
-    ledcWrite(left_motor_chanel_b, 0);
-    ledcWrite(right_motor_chanel_a, 0);
-    ledcWrite(right_motor_chanel_b, 0); 
+    ledcWrite(left_motor_channel_a, 0);
+    ledcWrite(left_motor_channel_b, 0);
+    ledcWrite(right_motor_channel_a, 0);
+    ledcWrite(right_motor_channel_b, 0); 
     Serial.println("stop");
 }
 
 void forward()
 {
-    ledcWrite(left_motor_chanel_a, vspeed_l);
-    ledcWrite(left_motor_chanel_b, 0);
-    ledcWrite(right_motor_chanel_a, vspeed_r);
-    ledcWrite(right_motor_chanel_b, 0);    
+    ledcWrite(left_motor_channel_a, vspeed_l);
+    ledcWrite(left_motor_channel_b, 0);
+    ledcWrite(right_motor_channel_a, vspeed_r);
+    ledcWrite(right_motor_channel_b, 0);    
     Serial.println("forward");
 } 
 
 void backward()
   {
-    ledcWrite(left_motor_chanel_a, 0);
-    ledcWrite(left_motor_chanel_b, (vspeed_l / 2));
-    ledcWrite(right_motor_chanel_a, 0);
-    ledcWrite(right_motor_chanel_b, (vspeed_r/2));    
+    ledcWrite(left_motor_channel_a, 0);
+    ledcWrite(left_motor_channel_b, vspeed_l);
+    ledcWrite(right_motor_channel_a, 0);
+    ledcWrite(right_motor_channel_b, vspeed_r);
     Serial.println("backward");
   } 
 
 void left()
 {
-    ledcWrite(left_motor_chanel_a, 0);
-    ledcWrite(left_motor_chanel_b, tspeed_l);
-    ledcWrite(right_motor_chanel_a, tspeed_r);
-    ledcWrite(right_motor_chanel_b, 0);      
+    ledcWrite(left_motor_channel_a, 0);
+    ledcWrite(left_motor_channel_b, tspeed_l);
+    ledcWrite(right_motor_channel_a, tspeed_r);
+    ledcWrite(right_motor_channel_b, 0);      
     Serial.println("left");
 }
 
 void right()
 {
-    ledcWrite(left_motor_chanel_a, tspeed_l);
-    ledcWrite(left_motor_chanel_b, 0);
-    ledcWrite(right_motor_chanel_a, 0);
-    ledcWrite(right_motor_chanel_b, tspeed_r);  
+    ledcWrite(left_motor_channel_a, tspeed_l);
+    ledcWrite(left_motor_channel_b, 0);
+    ledcWrite(right_motor_channel_a, 0);
+    ledcWrite(right_motor_channel_b, tspeed_r);  
     Serial.println("right");
 }
 
@@ -82,15 +86,21 @@ void setup() {
   ledcSetup(3, 5000, 8);
 
   // attach the channel to the GPIO to be controlled
-  ledcAttachPin(PWM_PIN_L_A, left_motor_chanel_a);
-  ledcAttachPin(PWM_PIN_L_B, left_motor_chanel_b);
-  ledcAttachPin(PWM_PIN_R_A, right_motor_chanel_a);
-  ledcAttachPin(PWM_PIN_R_B, right_motor_chanel_b);
+  ledcAttachPin(PWM_PIN_L_A, left_motor_channel_a);
+  ledcAttachPin(PWM_PIN_L_B, left_motor_channel_b);
+  ledcAttachPin(PWM_PIN_R_A, right_motor_channel_a);
+  ledcAttachPin(PWM_PIN_R_B, right_motor_channel_b);
 
   pinMode(W_LED_ON,OUTPUT);
   pinMode(IR_LED_ON,OUTPUT);
-  digitalWrite(W_LED_ON, 0);
+  digitalWrite(W_LED_ON, 1);
   digitalWrite(IR_LED_ON, 1);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+  }
 }
 
 void loop() {
@@ -99,7 +109,7 @@ void loop() {
   int sensor_3_state = analogRead(SENSOR_3_PIN);
   int sensor_4_state = analogRead(SENSOR_4_PIN);
 
-  Serial.print("sensor = \t");
+  Serial.print("ir = \t");
   Serial.print(sensor_1_state);
   Serial.print("\t");
   Serial.print(sensor_2_state);
@@ -107,6 +117,18 @@ void loop() {
   Serial.print(sensor_3_state);
   Serial.print("\t");
   Serial.println(sensor_4_state);
+
+  uint16_t r, g, b, c;
+  tcs.getRawData(&r, &g, &b, &c);
+
+  Serial.print("color (r-g-b-c) = \t");
+  Serial.print(r);
+  Serial.print("\t");
+  Serial.print(g);
+  Serial.print("\t");
+  Serial.print(b);
+  Serial.print("\t");
+  Serial.println(c);
 
   uint8_t sensor_array = 0;
   
@@ -123,13 +145,11 @@ void loop() {
     case 0b1000:
     case 0b1100:
       left();
-      delay(50);
       break;
 
     case 0b0001:
     case 0b0011:
       right();
-      delay(50);
       break;
 
     case 0b0110:
@@ -137,22 +157,19 @@ void loop() {
     case 0b0010:
     case 0b1110:
     case 0b0111:
+    case 0b1111:
       forward();
-      delay(50);
       break;
 
     case 0b0000:
-    case 0b1001:
       backward();
-      delay(50);
       break;
 
     default:
       forward();
-      delay(50);
       break;
   }
 
   Serial.println(sensor_array, BIN);
-  delay(1000);
+  delay(10);
 }
