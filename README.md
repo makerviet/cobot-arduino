bb60d711d5cdefb3a9ae8d48f05ff022e76dc78f
Bui Huu An <buihuuan5209@gmail.com>
2023-12-28 15:26:58 +0700

    final for publishing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# vorc
+# Tổng hợp mã nguồn và tài liệu hướng dẫn CoBot (phiên bản phát hành riêng cho VORC 2023)
+
+## [Hướng dẫn lắp ráp CoBot](https://youtu.be/J4CTvk9BN0E?si=wKa1nySzxEhOEeIE)
+
+## [Hướng dẫn lập trình CoBot](https://cms.vsteam.edu.vn/course/view.php?id=4)
+
+Toàn bộ tài nguyên được phát triển và đóng góp phi lợi nhuận bởi Maker Việt. Vui lòng trích dẫn nguồn khi chia sẻ!
---
+++ dev/rogo_bot_motors_test_pio/src/main.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+
+#define PWM_PIN_L_A 2
+#define PWM_PIN_L_B 10
+#define PWM_PIN_R_A 6
+#define PWM_PIN_R_B 5
+
+#define left_motor_chanel_a 0
+#define left_motor_chanel_b 1
+#define right_motor_chanel_a 2
+#define right_motor_chanel_b 3
+
+uint8_t vspeed = 255; //50    
+uint8_t tspeed = 255; //70
+
+uint8_t vspeed_l = vspeed;
+uint8_t tspeed_l = tspeed;
+uint8_t vspeed_r = vspeed * 1.0;
+uint8_t tspeed_r = tspeed * 1.0;
+
+
+void forward();
+void backward();
+void left();
+void stop();
+
+void setup() {
+  Serial.begin(115200);
+
+  ledcSetup(0, 5000, 8);
+  ledcSetup(1, 5000, 8);
+  ledcSetup(2, 5000, 8);
+  ledcSetup(3, 5000, 8);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(PWM_PIN_L_A, left_motor_chanel_a);
+  ledcAttachPin(PWM_PIN_L_B, left_motor_chanel_b);
+  ledcAttachPin(PWM_PIN_R_A, right_motor_chanel_a);
+  ledcAttachPin(PWM_PIN_R_B, right_motor_chanel_b);
+}
+
+void stop()
+{
+    ledcWrite(left_motor_chanel_a, 0);
+    ledcWrite(left_motor_chanel_b, 0);
+    ledcWrite(right_motor_chanel_a, 0);
+    ledcWrite(right_motor_chanel_b, 0);    
+    Serial.println("stop");
+}
+
+void forward()
+{
+    ledcWrite(left_motor_chanel_a, vspeed_l);
+    ledcWrite(left_motor_chanel_b, 0);
+    ledcWrite(right_motor_chanel_a, vspeed_r);
+    ledcWrite(right_motor_chanel_b, 0);    
+    Serial.println("forward");
+} 
+
+void backward()
+  {
+    ledcWrite(left_motor_chanel_a, 0);
+    ledcWrite(left_motor_chanel_b, vspeed_l);
+    ledcWrite(right_motor_chanel_a, 0);
+    ledcWrite(right_motor_chanel_b, vspeed_r);    
+    Serial.println("backward");
+  } 
+
+void left()
+{
+    ledcWrite(left_motor_chanel_a, 0);
+    ledcWrite(left_motor_chanel_b, tspeed_l);
+    ledcWrite(right_motor_chanel_a, tspeed_r);
+    ledcWrite(right_motor_chanel_b, 0);      
+    Serial.println("left");
+}
+
+void right()
+{
+    ledcWrite(left_motor_chanel_a, tspeed_l);
+    ledcWrite(left_motor_chanel_b, 0);
+    ledcWrite(right_motor_chanel_a, 0);
+    ledcWrite(right_motor_chanel_b, tspeed_r);  
+    Serial.println("right");
+}
+
+void loop() {
+  forward();
+
+    Serial.println(vspeed_l);
+    Serial.print(" ");
+    Serial.print(vspeed_r);
+
+  delay(3000);
+}
